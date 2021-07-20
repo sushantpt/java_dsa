acba8168a7d5ae9a4953c38f57ca6e9937894450
sushantpt <66266432+sushantpt@users.noreply.github.com>
2021-07-20 18:17:54 +0545

    Create insertionSort.java

---
+++ insertionSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		int [] dataSet = {/*j*/99, /*i*/123, 41, 1, 44, 234, 77, 7, 98, 54, 33, 1, 0}; // unsorted array
+		System.out.println("Unsorted Array:" + Arrays.toString(dataSet));
+		for (int i = 1; i < dataSet.length; i++) {
+			int tempData = dataSet[i];
+			int j = i - 1; 
+			while (j >= 0 && tempData <= dataSet[j]) {
+				dataSet[j + 1] = dataSet[j];
+				j = j - 1;
+			}
+			dataSet[j + 1] = tempData;
+		}
+		System.out.println("Sorted Array:" + Arrays.toString(dataSet));
+	}
+
+}
