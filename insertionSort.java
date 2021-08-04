95335ce6e620c542d31b8a3545d7312888873d37
sushantpt <66266432+sushantpt@users.noreply.github.com>
2021-08-04 18:49:46 +0545

    Insertion Sort

--- insertionSort.java
+++ insertionSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
 	public static void main(String[] args) {
-		int [] dataSet = {/*j*/99, /*i*/123, 41, 1, 44, 234, 77, 7, 98, 54, 33, 1, 0}; // unsorted array
+		int [] dataSet = {/*j*/99, /*i*/123, 41, 1, 44, 234, 77, 7, 98, 54, 33, 1, 0, 23, 345, 34, 1, 00}; // unsorted array
 		System.out.println("Unsorted Array:" + Arrays.toString(dataSet));
 		for (int i = 1; i < dataSet.length; i++) {
 			int tempData = dataSet[i];
