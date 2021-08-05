4e950843234b92cda4d861b200da751d2e3e2e79
sushantpt <66266432+sushantpt@users.noreply.github.com>
2021-08-05 21:36:57 +0545

    Linked List implementation

---
+++ LinkedList/src/linkedList/LinkedList.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class LinkedList {
+	private Node head;
+	
+	public void insertItem(int data) {
+		Node node = new Node();
+		node.data = data;
+	}
+
+}
---
+++ LinkedList/src/linkedList/Main.java
@@ -0,0 +1,9 @@
+package linkedList;
+
+public class Main {
+	
+	public static void main(String [] args) {
+		LinkedList linkedList = new LinkedList();
+		linkedList.insertItem(5);
+	}
+}
