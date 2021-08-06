5bc171115e6b2dc4b2b9e8fc5d6ddef281614019
sushantpt <66266432+sushantpt@users.noreply.github.com>
2021-08-06 21:36:51 +0545

    Linked List (Main method)

--- LinkedList/src/linkedList/Main.java
+++ LinkedList/src/linkedList/Main.java
@@ -1,1 +1,20 @@
 
+package linkedList;
+
+public class Main {
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.insert(534);
+		ll.insert(42);
+		ll.insert(134);
+		ll.insert(111);
+		ll.insert(123);
+		ll.insertAt(0, 45);
+//		ll.insertAt(15, 76);    				// NullPoint Exception. Reason: Total lenght of the list is less than index passed at insertAt() method.
+		ll.insertAtFirst(34);
+		ll.deleteAt(3);
+		
+		ll.showAll();
+		System.out.println("Lenght of the list is: " + ll.getLenght());
+	}
+}
